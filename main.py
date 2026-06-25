6bfa29de510722600e172c6dd529e2f0e7495def
SwiadomyDziwak <kojack26@gmail.com>
2026-06-25 23:50:42 +0200

    Recipe class - Added a method to show the dish's name - Added a method to show the dish's portions/servings - Added a method to show preparing steps - Added UI handling by allowing to set it inside the Recipe class
    UI translations
    - Added error messages related to adding properties to a recipe

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from weasyprint import HTML, CSS
 from recipe import Recipe
 import json
 from argparse import ArgumentParser
-from os import path
+from os import path, listdir
 
 def main() -> None:
 
@@ -18,26 +18,52 @@ def main() -> None:
     with open(target_ui_path) as ui_translation:
         ui: dict[str, str] = json.load(ui_translation)
 
-    with open("data/ziemniaczki_w_sosie_musztardowym.json") as data:
-        recipe_data: dict = json.load(data)
-    recipe: Recipe = Recipe()
-    recipe.set_dish_name(recipe_data["dish_name"])
-    recipe.set_servings(recipe_data["servings"])
-    for tag in recipe_data["tags"]:
-        recipe.add_tag(tag)
-    for category, ingredients in recipe_data["ingredients"].items():
-        recipe.add_ingredient_category(category)
-        for ingredient, amount in ingredients.items():
-            recipe.add_ingredient(category, ingredient, amount)
-    for step in recipe_data["preparing_steps"]:
-        recipe.add_preparing_step(step)
+    recipes_list: dict[str, str] = {}
+    no_of_recipes: int = 0
+    for file in listdir("data"):
+        filepath = path.join("data",file)
+        if path.isdir(filepath):
+            continue
+        no_of_recipes += 1
+        recipes_list[str(no_of_recipes)] = filepath
 
+    for recipe_nr, recipe_path in recipes_list.items():
+        print(f"{recipe_nr}: {recipe_path}")
+        
+    select: str = input("Select recipe: ")
+    if select in recipes_list.keys():
+        with open(recipes_list[select]) as data:
+            recipe_data: dict = json.load(data)
+        recipe: Recipe = Recipe()
+        recipe.ui = ui
+        recipe.set_dish_name(recipe_data["dish_name"])
+        recipe.set_servings(recipe_data["servings"])
+        for tag in recipe_data["tags"]:
+            recipe.add_tag(tag)
+        for category, ingredients in recipe_data["ingredients"].items():
+            recipe.add_ingredient_category(category)
+            for ingredient, amount in ingredients.items():
+                recipe.add_ingredient(category, ingredient, amount)
+        for step in recipe_data["preparing_steps"]:
+            recipe.add_preparing_step(step)
+    else:
+        print("Please enter a valid recipe number.")
+        return
+
+    print()
+    recipe.show_dish_name()
+    recipe.show_servings()
+    recipe.show_preparing_steps()
+    print()
+
+"""
     with open("templates/cooking1.html") as temp:
         template: Template = Template(temp.read())
     html: str = template.render(recipe=recipe, ui=ui)
     HTML(string=html).write_pdf(
             "pdfs/ziemniaczki_w_sosie_musztardowym.pdf",
             stylesheets=[CSS("templates/cooking1.css")])
+"""
 
 if __name__ == "__main__":
     main()
--- recipe.py
+++ recipe.py
@@ -1,17 +1,22 @@
 class Recipe:
     def __init__(self) -> None:
+        self.ui: dict[str, str] = None
+        self.separator: str = "-" * 15
         self.dish_name: str = None
         self.tags: list[str] = []
         self.servings: int = 0
         self.ingredients: dict[str, dict[str, str]] = {}
         self.preparing_steps: list[str] = []
 
+    # ---
+    # Setting and adding propeties
+
     def set_dish_name(self, name: str) -> None:
         self.dish_name = name
 
     def add_tag(self, tag: str) -> None:
         if tag in self.tags:
-            print("\033[91m[\u2718]\033[0m Tag already exists")
+            print(f"\033[91m[\u2718]\033[0m {self.ui['error_tag_exists']}")
             return
         self.tags.append(tag)
 
@@ -20,7 +25,7 @@ class Recipe:
 
     def add_ingredient_category(self, category: str) -> None:
         if category in self.ingredients.keys():
-            print("\033[91m[\u2718]\033[0m Category already exists.")
+            print(f"\033[91m[\u2718]\033[0m {self.ui['error_category_exists']}")
             return
         self.ingredients[category] = {}
 
@@ -28,8 +33,24 @@ class Recipe:
         try:
             self.ingredients[category][ingredient] = amount
         except KeyError:
-            print("033[91m[\u2718]\033[0m Wrong ingredient category/element.")
+            print(f"033[91m[\u2718]\033[0m {self.ui['error_wrong_category']}")
             return
 
     def add_preparing_step(self, step: str) -> None:
         self.preparing_steps.append(step)
+
+    # ---
+    # Showing data on call
+
+    def show_dish_name(self) -> None:
+        print(f"\033[94m-- {self.dish_name}\033[0m")
+
+    def show_servings(self) -> None:
+        print(f"\033[96mServings:\033[0m {self.servings}") 
+
+    def show_preparing_steps(self) -> None:
+        no_of_steps: int = 0
+        print(f"\033[96m{self.ui['preparing_steps']}\033[0m")
+        for step in self.preparing_steps:
+            no_of_steps += 1
+            print(f"{no_of_steps}. {step}")
